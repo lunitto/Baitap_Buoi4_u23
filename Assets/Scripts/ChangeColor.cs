8dd2e0de000f10db63d4bf3b805ab180665f1c3e
lunitto <31333882+lunitto@users.noreply.github.com>
2023-04-12 14:43:35 +0700

    bai tap lam duoc them

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Material[]materials;
+    public Renderer rend;
+    private int index = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        if (materials.Length == 0)
+        {
+            return;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            index += 1;
+            if (index == materials.Length + 1)
+            {
+                index = 1;
+            }
+            print (index);
+            rend.sharedMaterial = materials[index - 1];
+        }
+    }
+}
